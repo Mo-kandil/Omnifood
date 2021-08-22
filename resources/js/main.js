e81223e5f490a1ea250af39e4667628564c7c210
m emad <mrnoop22@gmail.com>
2021-08-22 09:42:36 +0200

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,8 @@
+// See https://www.npmjs.com/package/respond.js documentation.
+import * as respondJs from 'https://esm.run/respond.js';
+import * as html5shiv from 'https://esm.run/html5shiv';
+import * as selectivizr from 'https://esm.run/selectivizr';
+
+console.log(selectivizr)
+console.log(html5shiv)
+console.log(respondJs)
